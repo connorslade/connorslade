7dd9c97bf296fd75439f6346a7eafd9cd1b0d9e5
Connor Slade <connor@connorcode.com>
2021-07-31 13:07:41 -0400

    🗂️ Business Card

---
+++ ConnorSlade.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Connor Slade's Buisness Card :P
+
+colorPrint() {
+    echo "\033[$2m$1\033[0m"
+}
+
+colorPrint "Website | connorcode.com" 32
+colorPrint "Github  | Basicprogrammer10" 36
+colorPrint "Email   | connor@connorcode.com" 34
+colorPrint "GPG Key | connorcode.com/key.asc" 33
+colorPrint "Discord | Sigma#8214" 35
